d125516fa4f3cb4bf3974f434814c20ff7403d59
helga <helga>
2019-04-14 14:03:06 +0300

    feat: add solutions

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -30,7 +30,10 @@
  *
  */
 function getFizzBuzz(num) {
-    throw new Error('Not implemented');
+    if (num % 15 == 0 ) {return 'FizzBuzz'}
+    else if (num % 3 == 0) { return 'Fizz'}
+    else if (num % 5 == 0) { return 'Buzz'}
+    else return num
 }
 
 
@@ -46,7 +49,7 @@ function getFizzBuzz(num) {
  *   10 => 3628800
  */
 function getFactorial(n) {
-    throw new Error('Not implemented');
+    return n ? n * getFactorial(n - 1) : 1;
 }
 
 
@@ -63,7 +66,10 @@ function getFactorial(n) {
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
 function getSumBetweenNumbers(n1, n2) {
-    throw new Error('Not implemented');
+    let result = 0;
+
+    for(let i = n1; i <= n2 ; i++) { result += i};
+    return result
 }
 
 
@@ -82,7 +88,11 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,10,10 =>  true
  */
 function isTriangle(a,b,c) {
-    throw new Error('Not implemented');
+   
+    if ((a + b > c) && (b + c > a) && (c + a > b))
+    return true
+      else
+          return false
 }
 
 
